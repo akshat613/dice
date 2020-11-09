73abc5ab5d2ac248595bf5198642aa8f40558dcb
akshat613 <72006427+akshat613@users.noreply.github.com>
2020-11-09 10:12:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ var randomNumber1=Math.floor((Math.random() * 6) + 1);
 
 var randomDiceImg="dice"+randomNumber1+".png";
 
-var randomImgSource="images/"+randomDiceImg;
-
 var image1=document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",randomImgSource);
+image1.setAttribute("src",randomDiceImg);
 
 
 var randomNumber2=Math.floor((Math.random() * 6) + 1);
 
 var randomDiceImg2="dice"+randomNumber2+".png";
 
-var randomImgSource2="images/"+randomDiceImg2;
-
 var image2=document.querySelectorAll("img")[1];
 
-image2.setAttribute("src",randomImgSource2);
+image2.setAttribute("src",randomDiceImg2);
 
 
 if(randomNumber1>randomNumber2){
